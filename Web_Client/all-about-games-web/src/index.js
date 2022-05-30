5e722ab6d41768bb1cf7eda49c5a461c2820c40b
HristoTrendafilov <hristo.trendafilov93@gmail.com>
2022-05-30 19:28:10 +0300

    **

--- Web_Client/all-about-games-web/src/index.js
+++ Web_Client/all-about-games-web/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {RouteTable} from "./Infrastructure/RouteTable";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouteTable>
-
-    </RouteTable>
+    <App />
   </React.StrictMode>
 );
 
