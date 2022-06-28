348cb0e80bc81627d90f7aaa56e3962df200312e
Hristo Trendafilov <hristot@smart-ss.org>
2022-06-28 13:12:37 +0300

    nn

---
+++ WebClient/all-about-games-web/src/Roles/RolesForm.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+export function RolesForm(){
+    const [state, setState] = useState({isLoading: false, stateErrors: []});
+
+    return(
+        <div>roles...</div>
+    )
+}
